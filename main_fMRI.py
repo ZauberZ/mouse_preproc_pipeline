cc1766c0ef9e402cbca82db62a1aa3cb198aa6c6
ZauberZ <Zauber@outlook.com>
2025-07-03 17:49:12 +0800

    test

--- main_fMRI.py
+++ main_fMRI.py
@@ -1,17 +1,17 @@
 from utils.dce_pre import pre_dce, normalize_toTMP
 
 # subject path (it must contain MRI bold file, and the file suffix ends with .nii or .nii.gz)
-SUBJECT_DIR='/media/zzb/data/mouse/data/dce/'
+SUBJECT_DIR='/home/zzb/PycharmProjects/mouse_preproc_pipeline/test/'
 # The full name of the subject contains a suffix (The file name of MRI dce in SUBJECT_DIR)
 # e.g. dce.nii or dce.nii.gz
-SUBJECT_NAME='dce.nii'
-byMRI=True
+SUBJECT_NAME='bold.nii'
+byMRI=False
 MRI='T2w_0.07mm.nii'
 MRI_mask='T2w_mask.nii.gz'
-template_path='template/WHS/canon_T2W_brain_half_rez_oc.nii'
+template_path='template/WHS/canon_T1W_brain_half_rez_oc.nii'
 atlas_path='template/WHS/WHS_0.5_Labels_oc.nii'
 
 def main():
     pre_dce(SUBJECT_DIR,SUBJECT_NAME,byMRI,MRI)
-    # normalize_toTMP(SUBJECT_DIR,template_path,atlas_path,byMRI,MRI,MRI_mask)
+    normalize_toTMP(SUBJECT_DIR,template_path,atlas_path,byMRI,MRI,MRI_mask)
 main()
