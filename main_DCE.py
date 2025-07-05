72f892dcd98110bddc836df5afa69e7fb154222d
ZauberZ <Zauber@outlook.com>
2025-07-05 19:13:37 +0800

    2025.7.5 19:13

--- main_DCE.py
+++ main_DCE.py
@@ -5,14 +5,20 @@ SUBJECT_DIR='/media/zzb/data/mouse/data/dce/'
 # The full name of the subject contains a suffix (The file name of MRI dce in SUBJECT_DIR)
 # e.g. dce.nii or dce.nii.gz
 SUBJECT_NAME='dce.nii'
+# Normalize DCE to TMP
+isNorm=False
 # Reg by self-individual MRI
 byMRI=True
-MRI='T2w_0.07mm.nii'
+# T1w is an optional option. If T1w is not available, please set it to None.
+T1w='T1w_0.07mm.nii'
+# T2w is a required option.
+T2w='T2w_0.07mm.nii'
 # You can set brain template to reg
 template_path='template/WHS/canon_T2W_brain_half_rez_oc.nii'
 atlas_path='template/WHS/WHS_0.5_Labels_oc.nii'
 
+
 def main():
-    pre_dce(SUBJECT_DIR,SUBJECT_NAME,byMRI,MRI)
-    normalize_toTMP(SUBJECT_DIR,template_path,atlas_path,byMRI,MRI)
+    pre_dce(SUBJECT_DIR,SUBJECT_NAME,byMRI,T1w,T2w)
+    normalize_toTMP(SUBJECT_DIR,template_path,atlas_path,byMRI,T2w)
 main()
--- utils/dce_pre.py
+++ utils/dce_pre.py
@@ -10,12 +10,13 @@ import numpy as np
 from utils.util import meta_copy_4Dto3D
 from termcolor import colored
 
-def pre_dce(SUBJECT_DIR,fname,byMRI,MRI,step=0):
-    print('preproc dce')
+def pre_dce(SUBJECT_DIR,fname,byMRI,T1w,T2w,step=0):
+    print('Start to preproc dce')
     if step == 0:
         img = ants.image_read(SUBJECT_DIR + '/'+fname)
     else:
         img=ants.image_read(SUBJECT_DIR+'/dec_mc.nii.gz')
+    print('Motion correction')
     img_mc=ants.motion_correction(img)
     img_mc=img_mc['motion_corrected']
     if byMRI:
@@ -23,15 +24,34 @@ def pre_dce(SUBJECT_DIR,fname,byMRI,MRI,step=0):
         img_data = img_mc.numpy()
         average_tmp_data = np.mean(img_data[:,:,:,0:5], 3)
         avg=meta_copy_4Dto3D(img_mc,average_tmp_data)
-        mri=ants.image_read(SUBJECT_DIR+'/'+MRI)
-        mtmp=ants.image_read('template/MTMP/mouse_template_0.2mm.nii.gz')
-        mask = ants.image_read('template/MTMP/Brain_mask_0.2mm.nii.gz')
-        tt = ants.registration(mtmp, mri, 'SyN',reg_iterations=(40, 20, 20))
-        mask_=ants.apply_transforms(mri, mask, tt['invtransforms'], 'multiLabel')
-        mask_.to_file(SUBJECT_DIR+'/MRI_mask.nii.gz')
-        t=ants.registration(mri,avg,'Similarity')
+        if T1w is not None:
+            print('Correct dce by T1w')
+            mri=ants.image_read(SUBJECT_DIR+'/'+T1w)
+            t2=ants.image_read(SUBJECT_DIR+'/'+T2w)
+            if np.ndim(mri.numpy()) > 3:
+                mri_data = mri.numpy()
+                mri_avg = np.mean(mri_data, 3)
+                mri = meta_copy_4Dto3D(mri, mri_avg)
+            if np.ndim(t2.numpy()) > 3:
+                t2_data = t2.numpy()
+                t2_avg = np.mean(t2_data, 3)
+                t2 = meta_copy_4Dto3D(t2, t2_avg)
+            tmri=ants.registration(t2,mri,'Similarity',aff_metric='mattes')
+            mri = ants.apply_transforms(t2, mri, tmri['fwdtransforms'], 'bSpline')
+            mri.to_file(SUBJECT_DIR + '/T1w_aligntoT2w.nii.gz')
+        else:
+            print('Correct dce by T2w')
+            t2 = ants.image_read(SUBJECT_DIR + '/' + T2w)
+            mri=t2
+        t=ants.registration(mri,avg,'Similarity',aff_metric='mattes')
         t['warpedmovout'].to_file(SUBJECT_DIR+'/avg.nii.gz')
         img_mc_=ants.apply_transforms(mri,img_mc,t['fwdtransforms'],'bSpline',3)
+
+        mtmp=ants.image_read('template/MTMP/mouse_template_0.2mm.nii.gz')
+        mask = ants.image_read('template/MTMP/Brain_mask_0.2mm.nii.gz')
+        tt = ants.registration(mtmp, t2, 'SyN',reg_iterations=(40, 20, 0))
+        mask_=ants.apply_transforms(mri, mask, tt['invtransforms'], 'multiLabel')
+        mask_.to_file(SUBJECT_DIR+'/MRI_mask.nii.gz')
     else:
         img_data = img_mc.numpy()
         average_tmp_data = np.mean(img_data[:,:,:,0:5], 3)
@@ -39,9 +59,10 @@ def pre_dce(SUBJECT_DIR,fname,byMRI,MRI,step=0):
         avg.to_file(SUBJECT_DIR+'/avg.nii.gz')
         img_mc_=img_mc
     img_mc_.to_file(SUBJECT_DIR+'/dce_mc.nii.gz')
+    print(colored('DCE preproc end', "green"))
 
 def normalize_toTMP(SUBJECT_DIR,template_path,atlas_path,byMRI,MRI):
-    print(colored('normalize to template space, please wait...', "red"))
+    print(colored('Normalize to template space, please wait...', "red"))
     tmp=ants.image_read(template_path)
     atlas = ants.image_read(atlas_path)
     dce=ants.image_read(SUBJECT_DIR+'/dce_mc.nii.gz')
@@ -55,7 +76,8 @@ def normalize_toTMP(SUBJECT_DIR,template_path,atlas_path,byMRI,MRI):
     img_ = ants.apply_transforms(tmp, img, t['fwdtransforms'], interpolator='bSpline')
     atlas_=ants.apply_transforms(img,atlas,t['invtransforms'],interpolator='multiLabel')
     dce_ = ants.apply_transforms(tmp, dce, t['fwdtransforms'], interpolator='bSpline',imagetype=3)
-    atlas_.to_file(SUBJECT_DIR+'/atlas.nii.gz')
+    atlas_.to_file(SUBJECT_DIR+'/atlas_inDCE.nii.gz')
     img_.to_file(SUBJECT_DIR+'/img_inTMP.nii.gz')
-    dce_.to_file(SUBJECT_DIR+'/dce_inTMP.nii.gz')
-    print(colored('normalize to template space end', "green"))
+    dce_.to_file(SUBJECT_DIR+'/DCE_inTMP.nii.gz')
+    atlas.to_file(SUBJECT_DIR+'/atlas_inTMP.nii.gz')
+    print(colored('Normalize to template space end', "green"))
